9c3815a627f0879903a7dc5633e0cce3833ca339
DaveBackus <db3@nyu.edu>
2014-11-21 14:15:19 -0500

    cLASS #3

--- Code/Python/bootcamp_control.py
+++ Code/Python/bootcamp_control.py
@@ -1,7 +1,7 @@
 """
-For Class #2 of an informal mini-course at NYU Stern, Fall 2014.
+For Class #3 of an informal mini-course at NYU Stern, Fall 2014.
 
-Topics:  matplotlib graphics.  
+Topics:  control flow, functions 
 
 Repository of materials (including this file): 
 * https://github.com/DaveBackus/Data_Bootcamp 
@@ -19,9 +19,129 @@ c = a + b
 
 numbers = [x, y, z]
 strings = [a, b, c]
-all = numbers + strings
-print(all)
-print(all[:3] + all[3:]) 
+both = numbers + strings
+print(both)
+print(both[:3] + both[3:]) 
+
+[print(element) for element in both]
+[print(c[item]) for item in range(9)]
+#%%
+"""
+Conditional statements:  if, else, elif 
+"""
+# if statement 
+x = 7       # we can change this later and see what happens
+if x > 6:
+    square = x**2 
+    print('x**2 =', square)
+
+print('Done!')
 
 #%%
+# if and else statements  
+x = 5
+if x > 6:
+    square = x**2 
+    print('x**2 =', square)
+    print(x>6) 
+else:
+    print('x is not > 6 ( x =', x, ')')
+    print(x>6) 
+    
+print('Done!')
 
+#%%
+#%%
+"""
+Loops
+"""
+# simple loop
+for number in range(11):
+    square = number**2
+    print(square)
+
+#%%
+# Fibonacci numbers 
+print('\nFibonacci numbers')    # \n skips to the next line
+a, b = 0, 1
+for it in range(10):
+    a, b = b, a+b
+    print('At iteration', it, 'b =', b)
+
+#%%
+a, b = 0, 1
+ratio = a/b 
+maxit = 20 
+small_num = 1e-4
+for it in range(maxit):
+    a, b = b, a+b
+    new_ratio = a/b     
+    print('At iteration', it, ' ratio = ', new_ratio)
+    if abs(new_ratio-ratio) < small_num: 
+        break       # stop and exit loop break 
+    else:  
+        ratio = new_ratio     
+
+#%%
+# while loop 
+a, b = 0, 1
+ratio = a/b 
+maxit = 20 
+small_num = 1e-4
+error = 20 
+while error > small_num: 
+    a, b = b, a+b
+    new_ratio = a/b     
+    print('At iteration', it, 'ratio =', new_ratio)
+    error = abs(new_ratio-ratio)
+    ratio = new_ratio     
+
+#%%
+a, b = 0, 1
+while b < 100:
+    print('b =', b)
+    a, b = b, a+b
+    
+#%%
+# looping over strings and lists 
+vowels = 'aeiouy'
+word   = 'anything'
+for letter in word:  
+    if letter in vowels:
+        print(letter) 
+
+#%%
+# loop over list 
+fruit = ['apples', 'bananas', 'cherries']
+for item in fruit:
+    print(item)
+
+#%%
+fruit = ['apples', 'bananas', 'cherries']
+together = ''       # empty string
+for item in fruit:
+    print(item)
+    together = together + item 
+    
+print(together)    
+#%%
+
+# list comprehension
+[print(item) for item in fruit] 
+
+print('Done!')
+
+#%%
+"""
+Functions 
+"""
+# some other time 
+
+def fibonacci(a, b):
+    a, b = b, a+b
+    return a, b 
+
+a, b = 0, 1 
+a, b = fibonacci(a, b)    
+
+print(a, b)
---
+++ Code/Python/bootcamp_pandas.py
@@ -0,0 +1,93 @@
+"""
+For Class #3 of an informal mini-course at NYU Stern, Fall 2014.
+
+Topics:  pandas data management 
+
+Repository of materials (including this file): 
+* https://github.com/DaveBackus/Data_Bootcamp 
+
+Prepared by Dave Backus, Sarah Beckett-Hile, and Glenn Okun 
+Created with Python 3.4 
+"""
+import pandas as pd
+
+"""
+Examples of DataFrames
+"""
+# 538's income by college major 
+url1 = 'https://raw.githubusercontent.com/fivethirtyeight/'
+url2 = 'data/master/college-majors/recent-grads.csv'
+url = url1 + url2 
+df538 = pd.read_csv(url)
+
+#%%
+# check to see what we have 
+df538.head()        # list first five observations 
+df538.tail()        # last five 
+#%%
+# look at column names (.tolist() isn't necessary but easier to read)
+df538.columns
+df538.columns.tolist()
+
+df538.index.tolist()
+
+#%%
+# Fama-French equity return data 
+import pandas.io.data as web      
+ff = web.DataReader('F-F_Research_Data_Factors', 'famafrench')[0] 
+ff.columns = ['xsm', 'smb', 'hml', 'rf']
+
+#%%
+# constructing a DataFrame from scratch
+codes     = ['USA', 'FRA', 'JPN', 'CHN', 'IND', 'BRA', 'MEX'] 
+countries = ['United States', 'France', 'Japan', 'China', 'India', 
+             'Brazil', 'Mexico'] 
+# Wikipedia, 2013 USD 
+gdppc = [53.1, 36.9, 36.3, 11.9, 5.4, 15.0, 16.5]  # thousands
+gdp   = [16.8, 2.5, 4.7, 16.1, 6.8, 3.0, 2.1]  # trillions
+
+df = pd.DataFrame([gdp, gdppc, countries]).T
+
+#df.columns = ['GDP (trillions of USD)', 'GDP per capita (thousands of USD)',
+#              'Country Code']
+df.columns = ['gdp', 'gdppc', 'country'] 
+df.index = codes 
+
+#%%
+# Spencer's version
+# Warning:  uses a dictionary, skip if you don't know what that is 
+data = {"GDP (trillions of USD)": gdp,
+        "GDP per capita (thousands of USD)": gdppc,  
+        "Country": countries}
+dfspencer = pd.DataFrame(data, index=codes)
+
+#%%
+# Exercise 
+x = [1, 2, 5, 7, 10]
+y = [10, 7, 5, 2, 1]
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+
+"""
+Selecting variables and observations 
+"""
+# check 
+print(df) 
+
+some = df[0:3]
+print(some) 
+
+keep_obs = [0, 1] #['USA', 'BRA', 'JPN']
+keep_var = ['gdp', 'country']
+other = df[keep_obs]
+print(other)
+
+print(df[0:3])
+
+
+#%%
+"""
+Constructing new variables
+"""
+df['pop'] = df['gdp']/df['gdppc']
+print(df)
