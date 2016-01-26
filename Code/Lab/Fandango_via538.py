854aa70b4efdb5fd922881d97a43070f73fa684e
DaveBackus <db3@nyu.edu>
2016-01-26 17:57:09 -0500

    update code practice

---
+++ Code/Lab/Fandango_via538.py
@@ -0,0 +1,30 @@
+"""
+Fandango and other movie ratings  
+
+Links
+* http://fivethirtyeight.com/features/fandango-movies-ratings/
+* https://github.com/fivethirtyeight/data/tree/master/fandango
+
+Prepared for Data Bootcamp course at NYU  
+* https://github.com/DaveBackus/Data_Bootcamp
+* https://github.com/DaveBackus/Data_Bootcamp/Code/Lab 
+
+Written by Dave Backus, January 2016 
+Created with Python 3.5 
+"""
+import pandas as pd 
+#import matplotlib.pyplot as plt 
+
+url1 = 'https://raw.githubusercontent.com/fivethirtyeight/data/master/'
+url2 = 'fandango/fandango_score_comparison.csv'
+movie = pd.read_csv(url1+url2) 
+
+movie = movie.set_index('FILM')
+#movie.describe().T
+
+#%%
+# plots 
+movie.plot.scatter(x='IMDB', y='Fandango_Ratingvalue')
+movie.plot.scatter(x='Metacritic', y='Fandango_Ratingvalue')
+
+
