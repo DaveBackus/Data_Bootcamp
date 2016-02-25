d0ad18a42a4e5f0d26cc41beef1b6a55745120f8
DaveBackus <db3@nyu.edu>
2016-02-24 22:07:08 -0500

    update CP3

--- Code/Lab/NYC_open-data.py
+++ Code/Lab/NYC_open-data.py
@@ -3,6 +3,7 @@ NYC open data experiments
 
 NYU Open Data 
 https://nycopendata.socrata.com/
+https://dev.socrata.com/docs/endpoints.html 
 
 Directions:  search for dataset, click on export, right click on csv for url,
 then kill everything after the question mark. 
@@ -15,23 +16,28 @@ Written by Dave Backus, February 2016
 Created with Python 3.5
 """
 import pandas as pd
+import time
 
 #%%
 """
-
+restaurant inspections 
+takes 7-8 minutes with an ethernet connection, delivers 476k observations  
 """
-# restaurant inspections  
+start = time.process_time()
+print('\nLocal time at start:', time.localtime())
 url = 'https://nycopendata.socrata.com/api/views/xx67-kt59/rows.csv'
-insp = pd.read_csv(url, nrows=10)
+insp = pd.read_csv(url, low_memory=False)   
 
 print('\nNYC restaurant inspections')
 print('Dimensions:', insp.shape)
-print('Variables:', list(insp))
+print('Variables and types:\n', insp.dtypes, sep='')
 
+print('\nLocal time at finish:', time.localtime())
+print('\nTiming for restaraunt input', time.process_time()-start)
 
 #%%
 """
-https://nycopendata.socrata.com/data?browseSearch=311+complaints
+311 complaints 
 """
 url = 'https://data.cityofnewyork.us/api/views/erm2-nwe9/rows.csv'
 df311 = pd.read_csv(url, nrows=10)
@@ -39,3 +45,35 @@ df311 = pd.read_csv(url, nrows=10)
 print('\n311 calls')
 print('Dimensions:', df311.shape)
 print('Variables:', list(df311))
+
+#%%
+"""
+Itamar's 311 code 
+50k observations takes 7 minutes with ethernet connection
+Paul's explanation:  
+The data server runs a program that tells it how to deliver the data.
+The inputs to this program are passed to the server in the url after the ?.  
+The format is described in the SODA Query docs, which we access from the 
+wrench at the top of the NYC open data page:  
+https://nycopendata.socrata.com/
+https://dev.socrata.com/docs/queries/
+""" 
+print('\nLocal time at start:', time.localtime())
+
+size=1000
+start=0
+base_url='https://data.cityofnewyork.us/resource/erm2-nwe9.json'
+df_final=pd.DataFrame()
+for i in range(50):    
+    url = base_url+'?$limit=%s&$offset=%d'%(size,start)
+    df=pd.read_json(url)
+    df_final = df_final.append(df)
+    start+=size
+
+print('\n311 calls')
+print('Dimensions:', df_final.shape)
+print('Variables and types:\n', df_final.dtypes, sep='')
+
+print('\nLocal time at finish:', time.localtime())
+
+
--- Code/Lab/scratch_pad.py
+++ Code/Lab/scratch_pad.py
@@ -2,26 +2,147 @@
 """
 Created on Thu Feb 18 14:31:33 2016
 
+This is class 3 from databootcamp class
+
 @author: dbackus
 """
-#%%
-import pandas as pd
-import datetime as dt
+qualified = False
 
-print('Pandas version:', pd.__version__)
-print('Today:', dt.date.today())
 
 #%%
-url1 = 'https://raw.githubusercontent.com/DaveBackus'
-url2 = '/Data_Bootcamp/master/Code/Python/test.csv'
-url  = url1 + url2
-df = pd.read_csv(url)
-print(df)
-#print("df is a ", type(df))
+if qualified:
+    print('accept')
+else:
+    print('reject')
 
 #%%
-url1 = 'https://raw.githubusercontent.com/DaveBackus'
-url2 = '/Data_Bootcamp/master/Code/Python/test.xlsx'
-url  = url1 + url2
-pd.read_excel(url)
+print('Done!')
 
+#%%
+if 1<0:
+    print('1 is less than 0')
+
+
+#%%
+name1 = 'Chase'
+name2 = 'Dave'
+
+if name1<name2:
+    print(name1)
+else:
+    print(name2)
+
+#%%
+l = ['a', 'A', 'Chase', 'c', 'Glenn']
+sorted(l)
+
+#%%
+namelist = ["Chase", "Dave", "Sarah", "Spencer"]
+
+for item in namelist
+    print(item)
+
+#%%
+numlist = [4, -2, 5, 7, 1, 3, 4, -15, -20]
+total = 0
+n = len(numlist)
+
+for x in numlist:
+    total = total + x
+    
+print(total/n)
+
+#%%
+vowels = 'aeiouy'
+word = 'anything'
+
+for letter in word:
+    if (letter in vowels):
+        print('Vowel: ', letter)
+    else:
+        print('Consonant: ', letter)
+
+#%%
+stuff = ["Cat", 3.7, 5, "Dog"]
+
+#%%
+for thing in stuff:
+    print(thing)
+
+#%%
+for thing in stuff:
+    print(type(thing))
+    
+#%%
+for thing in stuff:
+    if type(thing) is str:
+        print(thing)
+
+#%%
+for el in range(5):
+    print(el)
+    
+for year in range(1980, 2016):
+    print(year)
+    
+#%%
+maturity = 5
+coupon = 5
+ytm = 0.05
+
+price = 0
+for year in range(1, maturity+1):
+    price = price + coupon/(1+ytm)**year
+    
+price = price + 100/(1+ytm)**maturity
+print('The price of the bond is: $', price)
+
+#%%
+fruit = ["apple", "banana", "clementine", "pineapple"]
+
+FRUIT = [item.upper() for item in fruit]
+
+#%%
+def hello(firstname):
+    print('Hello,', firstname)
+
+hello('Chase')
+hello(1)
+hello('Class')
+
+#%%
+def bondprice(maturity):
+    coupon = 5
+    ytm = 0.05
+
+    price = 0
+    for year in range(1, maturity+1):
+        price = price + coupon/(1+ytm)**year
+
+    price = price + 100/(1+ytm)**maturity
+    
+    return price
+    
+p = bondprice(5)
+print(p)
+
+#%%
+def older_than_30(age):
+    true_or_false = age >= 30
+    return true_or_false
+
+print(older_than_30(5))
+print(older_than_30(45))
+
+ages = range(50)
+for age in ages:
+    if older_than_30(age):
+        print(age, "is older than 30")
+
+#%%
+def nextyear(year):
+    return str(year+1)
+    
+print(nextyear(2015))
+
+#%%
