c1b79abea2a795d98649b969523ad21984e38d8b
DaveBackus <db3@nyu.edu>
2016-04-28 10:25:15 -0400

    seaborn and plotly after class

--- Code/Lab/JuTan_Yahoo_finance.py
+++ Code/Lab/JuTan_Yahoo_finance.py
@@ -8,7 +8,7 @@ from pandas.io.data import DataReader
 import datetime
 
 # small test sample
-start = datetime.datetime(2015, 12, 21)
+start = datetime.datetime(2012, 1, 1)
 end = datetime.datetime(2015, 12, 31)
 tickers = ['aapl', 'goog']
 
@@ -20,27 +20,17 @@ print('Dimensions:', p.shape)
 print('Axis labels:\n', p.describe, sep='')
 
 #%%
-close = p['Adj Close']
-c = close.stack().sortlevel(level=1).swaplevel(0,1)
-c
+# convert to df, shift tickers to columns
+df = p.to_frame().unstack('minor')['Adj Close']
+df.head(20)
 
 #%%
-# another version:  to_frame does the stacking automatically
-# can select variable at the start or the end
-df = p.to_frame().swaplevel(0,1).sortlevel(level=0)['Adj Close']
-df
+# convert to monthly (first day of month)
+dm = df.resample('MS')
+dm.head(20)
 
 #%%
-"""
-Ju's version (stripped down)
-"""
-d = {}
-for ticker in tickers:  #symbols_list:
-    d[ticker] = DataReader(ticker, "yahoo", start, end)
-
-pan = pd.Panel(d)
-df = pan.minor_xs('Adj Close')
-df
-
-#df.to_csv("yahoo closing price value.csv")
+# percent change based on start of month 
+test = dm.head()
+test.pct_change().shift(-1)
 
